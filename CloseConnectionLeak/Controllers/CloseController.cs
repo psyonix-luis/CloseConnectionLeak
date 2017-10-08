b465873610c9fc73ef5d3fea1e8890241f084770
Luis Mendez <luis.mendez@psyonix.com>
2017-10-08 00:27:31 -0700

    Initial commit

---
+++ CloseConnectionLeak/Controllers/CloseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CloseConnectionLeak.Controllers
+{
+	[Route("api/[controller]")]
+	public class CloseController : Controller
+	{
+		HttpSender HttpSender;
+
+		public CloseController(HttpSender httpSender) : base()
+		{
+			HttpSender = httpSender;
+		}
+
+		// GET api/close
+		[HttpGet]
+		public async Task<string> GetAsync()
+		{
+			var url = "test_post_url";
+			var responseBody = await HttpSender.HttpPostAsync(url, "Body");
+			return responseBody;
+		}
+	}
+}
---
+++ CloseConnectionLeak/HttpSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CloseConnectionLeak
+{
+    public class HttpSender
+    {
+		public HttpClient Client;
+
+		public HttpSender()
+		{
+			Client = new HttpClient();
+			Client.DefaultRequestHeaders.ConnectionClose = true;
+		}
+
+		public async Task<String> HttpPostAsync(string url, string body)
+		{
+			HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, url) ;
+			using (var resp = await Client.SendAsync(requestMessage))
+			{
+				if (resp.IsSuccessStatusCode)
+				{
+					var val = await resp.Content.ReadAsStringAsync();
+					return val;
+				}
+			}
+			return "error";
+		}
+	}
+}
